b5f6aa966a6f106764dbdcc7c01133b27cc80d2c
Anastasiya <ephimova.ad@gmail.com>
2023-05-30 01:33:16 +0400

    Set deployment

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-import router from "./routes.js";
+import router from "../src/routes.js";
 app.use(router);
 
 app.listen(80, () => console.log("Server has been started..."));
