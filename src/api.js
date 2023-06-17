b7de2dff80ba85724ac41e265a1edaa41e79f081
Anastasiya <ephimova.ad@gmail.com>
2023-06-17 17:25:17 +0400

    change cors setting

--- src/api.js
+++ src/api.js
@@ -4,11 +4,24 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 
-const corsOptions = {
-  origin: "http://localhost:8080",
-};
-
-app.use(cors(corsOptions));
+var allowedOrigins = [
+  "http://localhost:8080",
+  "https://english-cards-zeta.vercel.app/",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 import router from "./routes.js";
 app.use(router);
