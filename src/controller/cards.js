aa927ad632f78edeb5498f3ae399bbe2ff6fcee9
Anastasiya <ephimova.ad@gmail.com>
2023-06-21 20:31:17 +0400

    Доделан getDataFromAi

--- src/controller/cards.js
+++ src/controller/cards.js
@@ -81,7 +81,7 @@ export async function insertGroup(req, res) {
 }
 
 export async function getDataFromAi(req, res) {
-  const wordList = req.body.word_list;
+  const { wordList, groupId } = req.body;
   const content = `Составь из списка слов json массив объектов с полями englishWord, russianWord, englishExample, russianExample. Где englishWord - это анлийское слово, russianWord -  перевод на русский (если переводов несколько, то нужно вывести основные через запятую), englishExample - пример использования в предложении,  russianExample - перевод примера на русский. Пришли только json без комментариев и без символов. Вот список: ${wordList}.`;
   try {
     const completion = await openai.createChatCompletion({
@@ -92,19 +92,31 @@ export async function getDataFromAi(req, res) {
       ],
     });
 
-    const arr = JSON.parse(completion.data.choices[0].message.content);
-    const isArr = Array.isArray(arr);
+    const data = JSON.parse(completion.data.choices[0].message.content);
+
+    const isArr = Array.isArray(data);
+
+    if (!isArr) {
+      res.status(500).send("Ошибка генерации данных. Попробуйте снова");
+      return;
+    }
+
+    const wordSet = data.map((item) => ({
+      ...item,
+      groupId,
+      association: "",
+      done: false,
+      picture: "https://cdn-icons-png.flaticon.com/512/3983/3983886.png",
+    }));
 
     await client.connect();
     const words = database.collection("words");
 
-    await words.insertMany(arr);
+    await words.insertMany(wordSet);
 
     res.json({
       statusCode: 200,
     });
-
-    // res.json({ isArr, arr });
   } catch (error) {
     res.status(500).send(error);
   } finally {
